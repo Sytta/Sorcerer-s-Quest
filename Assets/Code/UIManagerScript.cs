6f91483f767d74acb996d2ef3d912aa96dd1e653
Nathalie Deviana Bekaert <Nathalie.Bekaert@gmail.com>
2016-11-29 20:21:36 -0500

    Added : Basic menu

---
+++ Assets/Code/UIManagerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public void StartGame() {
+		Application.LoadLevel("Scene");
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+
+	public void ChangeVolume() {
+		AudioListener.volume = GameObject.Find ("Volume Slider").GetComponent <Slider> ().value;;
+	}
+}
