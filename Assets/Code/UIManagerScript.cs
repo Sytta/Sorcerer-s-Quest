1671bee6f0bd3072966456ce9a132a2cc914dcd5
Nathalie Deviana Bekaert <Nathalie.Bekaert@gmail.com>
2016-12-04 14:32:39 -0500

    Modified : Help page

--- Assets/Code/UIManagerScript.cs
+++ Assets/Code/UIManagerScript.cs
@@ -7,16 +7,6 @@ public class UIManagerScript : MonoBehaviour {
 
 	public AudioClip buttonPress;
 
-	void Start() {
-		GameObject.Find ("TextPowerUps").GetComponent<Text> ().enabled = false;
-		GameObject.Find ("TextPowerUps2").GetComponent<Text> ().enabled = false;
-		GameObject.Find ("previous_btn").GetComponent<Button> ().enabled = false;
-		GameObject.Find ("money_img").GetComponent<Image> ().enabled = false;
-		GameObject.Find ("invincibility_img").GetComponent<Image> ().enabled = false;
-		GameObject.Find ("stop_img").GetComponent<Image> ().enabled = false;
-		GameObject.Find ("life_img").GetComponent<Image> ().enabled = false;
-	}
-
 	public void StartGame() {
 		AudioSource.PlayClipAtPoint (buttonPress, new Vector3(0.0f,0.0f,0.0f));
 		Application.LoadLevel("Scene");
@@ -43,28 +33,5 @@ public class UIManagerScript : MonoBehaviour {
 		AudioSource.PlayClipAtPoint (buttonPress, new Vector3(0.0f,0.0f,0.0f));
 		Application.LoadLevel ("Help");
 	}
-
-	public void nextPage() {
-		GameObject.Find ("TextStory").GetComponent<Text> ().enabled = false;
-		GameObject.Find ("next_btn").GetComponent<Button> ().enabled = false;
-		GameObject.Find ("TextPowerUps").GetComponent<Text> ().enabled = true;
-		GameObject.Find ("TextPowerUps2").GetComponent<Text> ().enabled = true;
-		GameObject.Find ("previous_btn").GetComponent<Button> ().enabled = true;
-		GameObject.Find ("money_img").GetComponent<Image> ().enabled = true;
-		GameObject.Find ("invincibility_img").GetComponent<Image> ().enabled = true;
-		GameObject.Find ("stop_img").GetComponent<Image> ().enabled = true;
-		GameObject.Find ("life_img").GetComponent<Image> ().enabled = true;
-	}
-
-	public void previousPage() {
-		GameObject.Find ("TextStory").GetComponent<Text> ().enabled = true;
-		GameObject.Find ("next_btn").GetComponent<Button> ().enabled = true;
-		GameObject.Find ("TextPowerUps").GetComponent<Text> ().enabled = false;
-		GameObject.Find ("TextPowerUps2").GetComponent<Text> ().enabled = false;
-		GameObject.Find ("previous_btn").GetComponent<Button> ().enabled = false;
-		GameObject.Find ("money_img").GetComponent<Image> ().enabled = false;
-		GameObject.Find ("invincibility_img").GetComponent<Image> ().enabled = false;
-		GameObject.Find ("stop_img").GetComponent<Image> ().enabled = false;
-		GameObject.Find ("life_img").GetComponent<Image> ().enabled = false;
-	}
+		
 }
